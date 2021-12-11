e0c6d3676e1bb714bd001ec89c2e5f4db377406a
amit thakur <amithakur081@gmail.com>
2021-12-11 22:06:55 +0530

    Add : loader

---
+++ src/component/loader/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+function Loader() {
+    return (
+        <div className ="loader"><img src="https://acegif.com/wp-content/uploads/loading-80.gif" /></div>
+        
+    )
+}
+
+export default Loader
