4bc6ae43ee0993878db1525ce94daf3ab76de17d
amit thakur <amithakur081@gmail.com>
2021-12-11 22:04:01 +0530

    Style : header

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,30 +1,86 @@
 import React from 'react'
 import Logo from "../../media/meeteor.png"
 import "./style.css"
-import { FiSearch } from "react-icons/fi";
+// import { FiSearch } from "react-icons/fi";
+import {useSelector} from "react-redux"
 import { RiAliensFill } from "react-icons/ri";
-import {Link} from "react-router-dom"
-
+import {Link,useNavigate} from "react-router-dom"
+import {useState} from "react"
+import {logout} from "../../features/user/userSlice";
+import {useDispatch} from "react-redux"
 function Header() {
+    const dispatch = useDispatch()
+
+    const [searchText , setSearchText] =useState("")
+    const [userOption, setUserOption] =useState(false)
+    const {user ,otherUsers} = useSelector((state)=>state.user)
+
+    const navigate = useNavigate()
+    const searchData = otherUsers.filter((data)=> data.username.match(searchText))
+    const logoutHandler =()=>{
+        dispatch(logout())
+        setUserOption(false);
+        // navigate("/login")
+    }
+
     return (
         <nav className ="header">
             <Link to="/">
             <section className ="logo">
                 <img src={Logo} alt="logo"/>
-                <div className ="logoName">Meet<br/><span>Your</span></div>
+                <div className ="logoName">Meet<span>Your</span></div>
             </section>
             </Link>
             <section className ="subHeader">
             <div className ="searchBar">
-                    <input/>
-                    <FiSearch/>
+                
+                <input value={searchText} placeholder = "Search..." onChange ={(e)=>setSearchText(e.target.value)}/>
+                {searchText.length > 0 && <div className ="searchResult">{
+                    searchData.map((user)=>{
+                        return (
+                            <>
+                            <Link to ={`/profile/${user._id}`}>
+                            <div className="searchItem" onClick={()=>{
+                                setSearchText("")
+                                }}>
+                                <img src={user.displayPic}/>
+                                <span>{user.username}</span>
+                            </div>
+
+                            </Link>
+                            </>
+                        )
+
+                    })
+                
+
+                }</div>}
+                {(searchText.length > 0 && searchData.length === 0 ) && <div className ="searchResult" style={{fontSize : "18px"}}>No match</div>}
+                    
             </div>
             <section className ="headerOption">
                 
-            <Link to ="/register" ><button className="logBtn">Log out</button></Link>
-                <Link to ="/profile" >
+            {/* <Link to ="/register" ><button className="logBtn">Log out</button></Link> */}
+                
+                
+                <div className ="userMenu">
+                <span onClick={()=>{
+                    setUserOption((menu)=>!menu
+                    )
+
+                }}>
                 <RiAliensFill />
+                </span>
+                {userOption && <div className ="userMenu__options umOption__on" >
+                <Link to ={`/profile/${user._id}`} >
+                    <span onClick={()=>setUserOption(false)}>Profile</span>
                 </Link>
+                <span onClick={()=>logoutHandler()}>Log out</span>
+                </div>}
+
+                </div>
+
+                
             </section>
             </section>
 
