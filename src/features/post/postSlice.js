81be32554d929f96fb85066122891d019212af9b
amit thakur <amithakur081@gmail.com>
2021-12-11 22:00:29 +0530

    Set up redux toolkit

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,9 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import axios from 'axios'
+
+export const getPosts = createAsyncThunk('post/getPosts',async ()=>{
+    const response = await axios.get("post/all");
+    
+    return response.data.data
+})
+
+export const addPost = createAsyncThunk("post/add", async(postData)=>{
+    console.log(postData)
+    const response = await axios.post("post/addPost",postData);
+    return response.data.data
+})
+
+export const likePost = createAsyncThunk("post/like",async(postId)=>{
+    const response = await axios.post(`post/like/${postId}`);
+    console.log(response.data.data.like)
+    return {response : response.data.data.like , postId : postId }
+})
+
+export const addComment = createAsyncThunk("post/comment",async({postId, commentText})=>{
+    console.log(commentText)
+    const response = await axios.post(`comment/${postId}/add`,{commentText : commentText});
+    console.log(response.data.data.comment)
+    return {response : response.data.data.comment , postId : postId }
+})
+
+
 
 
 const initialState = {
-    posts : [
-    ]
+    posts : [],
+    status : 'loading'
 }
 
 const postSlice = createSlice({
@@ -13,6 +41,40 @@ const postSlice = createSlice({
         uploadPost : (state,action)=>{
             return state
         }
+    },
+    extraReducers :{
+        [getPosts.pending]: (state,action)=>{
+            state.status ='loading'
+        },
+        [getPosts.fulfilled] : (state,{payload})=>{
+            state.posts = payload
+            state.status = 'success'
+        },
+        [getPosts.rejected] : (state,action)=>{
+            state.status = "failed"
+        },
+        [addPost.pending]: (state,action)=>{
+            state.status ='loading'
+        },
+        [addPost.fulfilled]:(state,{payload})=>{
+
+            state.posts.unshift(payload)
+            state.status = "success"
+        },
+        [addPost.rejected]: (state,action)=>{
+            state.status ='error'
+        },
+        [likePost.fulfilled] : (state,{payload})=>{
+            const findPost = state.posts.findIndex((post)=>post._id === payload.postId)
+            state.posts[findPost].like = payload.response
+            
+        },
+        [addComment.fulfilled] :(state,{payload})=>{
+            const findPost = state.posts.findIndex((post)=>post._id === payload.postId);
+            state.posts[findPost].comments.unshift(payload.response)
+        }
+
+
     }
 })
 
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,135 @@
+import {createSlice , createAsyncThunk}  from "@reduxjs/toolkit";
+import axios from 'axios'
+import {toast} from "react-toastify"
+const initialState = {
+    user : [],
+    otherUsers : [],
+    otherUserProfile : {},
+    status : 'loading',
+    login : localStorage?.getItem('auth') ? true : false,
+
+}
+
+export const register = createAsyncThunk("/user/register",async(userData)=>{
+    const response = await axios.post("/auth/register",userData);
+    console.log(response.data.data.token);
+    return response.data.data
+
+})
+
+export const login = createAsyncThunk("user/login",async(userData)=>{
+    const response = await axios.post("/auth/login",userData);
+    console.log(response.data.data.token)
+    return response.data.data
+
+})
+
+export const fetchUser = createAsyncThunk("/user/detail",async()=>{
+    const response = await axios.get("/user/userProfile");
+    return response.data.data
+
+})
+
+export const allUsers = createAsyncThunk("/user/all",async()=>{
+    const response = await axios.get("/user/all");
+    console.log(response.data.data)
+    return response.data.data
+})
+
+export const changeProfilePic = createAsyncThunk("/user/profile_pic",async(displayPic)=>{
+    const response =  await axios.post("/user/changepic",{displayPic})
+    return response.data.data
+})
+
+
+
+
+export const followUser = createAsyncThunk("user/follow",async({toFollow})=>{
+
+    const response = await axios.post(`user/follow/${toFollow}`);
+
+})
+
+export const seeProfile = createAsyncThunk("user/profile",async({userId})=>{
+    const response = await axios.get(`user/profile/${userId}`)
+    return response.data.data
+})
+const userSlice = createSlice({
+    name : "user",
+    initialState,
+    reducers : {
+        logout : (state,action)=>{
+            localStorage.removeItem('auth')
+            state.login = false;
+        }
+    },
+    extraReducers : {
+        [register.pending]:(state,action)=>{
+            state.status ="loading"
+        },
+        [register.fulfilled]:(state,{payload})=>{
+            state.user = payload.data;
+            state.login = true;
+            localStorage.setItem("auth",JSON.stringify({token : payload.token , login : true}))
+            state.status = "success"
+        },
+        [register.rejected]:(state,action)=>{
+            state.status = "failed"
+        },
+        [login.pending]:(state,{payload})=>{
+            state.status ="loading"
+            
+        },
+        [login.fulfilled]:(state,{payload})=>{
+            state.user = payload.data;
+            state.login =true
+            localStorage.setItem("auth",JSON.stringify({token : payload.token , login : true}))
+            state.status = "success"
+        },
+        [login.rejected]:(state,{error})=>{
+            console.log(error.message)
+            state.status = "failed"
+            toast.error("user not found")
+
+        },
+        [fetchUser.pending]:(state,action)=>{
+            state.status ="loading"
+        },
+        [fetchUser.fulfilled]:(state,{payload})=>{
+            state.user = payload;
+            state.status = "success"
+            state.login=true
+        },
+        [fetchUser.rejected]:(state,action)=>{
+            state.status = "failed"
+        },
+        [changeProfilePic.pending]:(state,action)=>{
+            state.status ="loading"
+        },
+        [changeProfilePic.fulfilled]:(state,{payload})=>{
+            state.user.displayPic = payload.displayPic;
+            state.status = "success"
+        },
+        [changeProfilePic.rejected]:(state,action)=>{
+            state.status = "failed"
+        },
+        [allUsers.fulfilled]:(state,{payload})=>{
+            state.otherUsers = payload;
+        },
+        [seeProfile.pending]:(state,action)=>{
+            // state.status ="loading"
+        },
+        [seeProfile.fulfilled]:(state,{payload})=>{
+            console.log(payload)
+            state.otherUserProfile = payload
+            state.status = "success"
+        },
+        [seeProfile.rejected]:(state,action)=>{
+            state.status = "failed"
+        },
+        
+    }
+})
+
+export const {logout} = userSlice.actions
+export default userSlice.reducer 
