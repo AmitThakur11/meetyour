e201bb6f4923fc95b80d9ed8fbe86c0ef4c1131b
amit thakur <amithakur081@gmail.com>
2021-12-11 22:11:01 +0530

    add private routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import React from 'react';
-import './index.css';
-import Header from "./component/header/index"
-import Home from './pages/home';
-import Profile from './pages/profile/index';
-import Register from "./pages/register"
-import Login from "./pages/login"
-import {Routes ,Route} from "react-router-dom"
+import { useEffect } from "react";
+import "./index.css";
+import Header from "./component/header/index";
+import Home from "./pages/home";
+import Profile from "./pages/profile/index";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import ShowPost from "./pages/ShowPost";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { fetchUser ,allUsers} from "./features/user/userSlice";
+import { getPosts } from "./features/post/postSlice";
+
+import PrivateRoute from "./features/user/privateRoute"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+
 function App() {
+  const dispatch = useDispatch();
+
+  axios.defaults.baseURL = "http://localhost:8000";
+  axios.defaults.headers.common["Authorization"] = JSON.parse(
+    localStorage?.getItem("auth")
+  )?.token;
+
+  const { user ,status,login } = useSelector((state) => state.user);
+
+
+
+  useEffect(() => {
+    
+    (async () => {
+      dispatch(getPosts());
+      let isLogin = JSON.parse(localStorage?.getItem("auth"))?.login;
+      if(isLogin){
+        dispatch(fetchUser());
+        dispatch(allUsers())
+      }
+      
+    })();
+  }, [dispatch,login]);
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
+
       <Routes>
-        <Route path ="/" element = {<Home/>}/>
-        <Route path ="/login" element={<Login/>} />
-        <Route path ="/register" element = {<Register/>}/>
-        <Route path ="/profile" element={<Profile/>}/>
-      </Routes>
+          <PrivateRoute  exact path="/" element={<Home/>} />
+          <PrivateRoute   path="/profile/:userId" element={<Profile />} />
+          <PrivateRoute   path="/post/:postId" element={<ShowPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
       
+      </Routes>
+
+        
+      <ToastContainer/>
+
     </div>
   );
 }
