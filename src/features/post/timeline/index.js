efef0da0d02bbbd0aefdd8053f7bdf84fb8df413
amit thakur <amithakur081@gmail.com>
2021-12-11 22:12:25 +0530

    made responsive

---
+++ src/features/post/timeline/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import AddPost from "../addPost";
+import PostCard from "../postCard";
+import "./style.css"
+import Loader from "../../../component/loader";
+import { useSelector } from "react-redux";
+function Timeline() {
+  let { posts, status } = useSelector((state) => state.post);
+  return (
+    <section className="timeline">
+      {status === "loading" && <Loader />}
+      {status === "success" && (
+        <>
+          <AddPost />
+          <section className="postListSection">
+            {posts.map((post) => {
+              return <PostCard post={post} />;
+            })}
+          </section>
+        </>
+      )}
+      {status === "error" && <h1>error</h1>}
+    </section>
+  );
+}
+
+export default Timeline;
