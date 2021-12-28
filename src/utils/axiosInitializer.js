413c81b9eb0ffae8cf2a530b0ff23e942d60f319
amit thakur <amithakur081@gmail.com>
2021-12-29 00:21:16 +0530

    add api

--- src/utils/axiosInitializer.js
+++ src/utils/axiosInitializer.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export  default function AxiosInitializer(){
-    axios.defaults.baseURL = "http://localhost:8000";
+    axios.defaults.baseURL = "https://meetyourbackend.herokuapp.com/";
     axios.defaults.headers.common["Authorization"] = JSON.parse(localStorage?.getItem("auth"))?.token;
 }
