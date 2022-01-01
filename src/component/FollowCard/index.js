cc8c9da433085c9fc737c106835bd67a9dbf50df
amit thakur <amithakur081@gmail.com>
2022-01-01 15:16:20 +0530

    fix : follow himself

--- src/component/FollowCard/index.js
+++ src/component/FollowCard/index.js
@@ -1,36 +1,46 @@
 import "./style.css";
-import { useDispatch , useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { followUser } from "../../features/user/userSlice";
-import {Link}from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TiPlus } from "react-icons/ti";
 function FollowCard(props) {
-
   const dispatch = useDispatch();
-  const {user} = useSelector((state)=>state.user)
-  const label = user?.following?.find(({_id})=>_id === props.user._id)
+  const { user } = useSelector((state) => state.user);
+  const label = user?.following?.find(({ _id }) => _id === props.user._id);
   return (
-      <>
-      
-    <div keys={props.user._id} className="followCard">
-      <div className="fc_one">
-      <Link to={`/profile/${props.user._id}`}>
-        <img className="fc_userImg" src={props.user.displayPic} alt="user" />
-      </Link>
+    <>
+      <div keys={props.user._id} className="followCard">
+        <div className="fc_one">
+          <Link to={`/profile/${props.user._id}`}>
+            <img
+              className="fc_userImg"
+              src={props.user.displayPic}
+              alt="user"
+            />
+          </Link>
+        </div>
+
+        <div className="fc_two">{props.user.username}</div>
+        <div className="fc_three">
+          {props.user._id !== user._id && (
+            <button
+              className="fc_followBtn"
+              onClick={() => dispatch(followUser({ toFollow: props.user._id }))}
+            >
+              {label ? (
+                <span>Unfollow </span>
+              ) : (
+                <span>
+                  {" "}
+                  Follow <TiPlus />
+                </span>
+              )}
+            </button>
+          )}
+        </div>
       </div>
-    
-      <div className="fc_two">{props.user.username}</div>
-      <div className="fc_three">
-        <button
-          className="fc_followBtn"
-          onClick={() => dispatch(followUser({ toFollow: props.user._id }))}
-        >
-          {label ? <span>Unfollow  </span>: <span> Follow <TiPlus/></span> }
-        </button>
-      </div>
-    </div>
     </>
   );
-  
 }
 
 export default FollowCard;
