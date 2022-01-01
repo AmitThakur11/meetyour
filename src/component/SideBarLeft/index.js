06d620d6ca48d9e2f5ab0d0768a6eb95faa8c9fe
amit thakur <amithakur081@gmail.com>
2022-01-01 15:38:48 +0530

    fix sidebar image

--- src/component/SideBarLeft/index.js
+++ src/component/SideBarLeft/index.js
@@ -6,13 +6,12 @@ import SideMenuBtn from '../SideMenuBtn/index'
 import {RiUserFollowLine , RiImageLine , RiBookmarkLine} from 'react-icons/ri'
 import {Link} from "react-router-dom"
 function SideBarLeft({on}) {
-    let {user} =  useSelector((state)=>state.user);
+    let {user,status} =  useSelector((state)=>state.user);
 
     return (
         <section className = {on?"leftSideBar__container bar-one barOn":"leftSideBar__container bar-one"}>
-        <Link to = {`/profile/${user._id}`} ><img  className ="leftSideBar__img" src = {user.displayPic} alt ="user_pic"/></Link>
+        <Link to = {`/profile/${user._id}`} >{status==="success"?<img  className ="leftSideBar__img" src = {user.displayPic} alt ="user_pic"/>:<div className ="leftSideBar__img"></div>}</Link>
         <div className ="leftSideBar">
-            {/* <SideMenuBtn name ="Home" icon = {<RiHome5Line/>} route ="/"/> */}
             <SideMenuBtn name ="Followers" icon = {<RiUserFollowLine/>} route ="/followers"/>
             <SideMenuBtn name ="Following" icon = {<RiUserFollowLine/>} route = "/following"/>
             <SideMenuBtn name ="Post" icon = {<RiImageLine/>} route = "/posts"/>
