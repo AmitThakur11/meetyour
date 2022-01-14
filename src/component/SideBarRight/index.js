0dc48fd52a4c9382c8433c84188e09aefaba06ff
amit thakur <amithakur081@gmail.com>
2022-01-14 19:51:03 +0530

    Refactor

--- src/component/SideBarRight/index.js
+++ src/component/SideBarRight/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect} from "react";
 import FollowCard from "../FollowCard";
 import { useDispatch, useSelector } from "react-redux";
 import { allUsers } from "../../features/user/userSlice";
@@ -8,19 +8,23 @@ import { useNavigate } from "react-router";
 
 function SideBarRight({ on }) {
   const dispatch = useDispatch();
+
   const { user, otherUsers } = useSelector((state) => state.user);
-  const explore =(others)=>{
-    const userIds = otherUsers.map(({_id})=>_id).flat();
-    return others._id !== user._id && userIds.includes(others._id)
+
+  const explore = ()=>{
+    const userIds = user.following?.map((data)=>data._id)
+    const notFollowed = otherUsers.filter((otherUser)=> user._id !== otherUser._id && !userIds?.includes(otherUser._id))
+    return notFollowed
    
   }
+  const exploreData = explore()
+
 
 
   const navigate = useNavigate();
-  useEffect(() => {
-    dispatch(allUsers());
-  }, [dispatch]);
-
+  useEffect(()=>{
+    dispatch(allUsers())
+  },[dispatch])
   return (
     <section
       className={on ? "rightSideBar bar-two barOn" : "rightSideBar bar-two "}
@@ -28,14 +32,11 @@ function SideBarRight({ on }) {
       <div className="exploreSection">
         <MdOutlineExplore />
       </div>
-
-      {otherUsers.map(
-        (others, index) =>
-          index < 3 &&
-          explore(others) && (
-            <FollowCard key={others._id} user={others} />
-          )
-      )}
+      {
+        exploreData.map((explore,index)=>{
+          return index < 3 &&  <FollowCard key={explore._id} user={explore} />
+        })
+      }
       <button className="exploreBtn" onClick={() => navigate("/explore")}>
         See more
       </button>
